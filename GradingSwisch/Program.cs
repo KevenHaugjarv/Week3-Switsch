a23024688348b33a284ba85c4c7273a466634101
unknown <khaugjar@tktk.ee>
2022-02-13 19:58:43 +0200

    add new projects

---
+++ GradingSwisch/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace GradingSwisch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Programm küsib, mis hinde kasutaja sai;
+            // kui kasutaja sai "A" konsool kuvab "Suurepärane";
+            // kui kasutaja sai "B" konsool kuvab "Väga hea";
+            // kui kasutaja sai "C" konsool kuvab "Hea";
+            // kui kasutaja sai "D" konsool kuvab "Rahuldav";
+            // kui kasutaja sai "E" konsool kuvab "Kasin";
+            // kui kasutaja sai "F" konsool kuvab "Puudulik";
+            // kui kasutaja sisestab midagi muud, siis konsool kuvab "vale väärtus";
+
+            Console.WriteLine("Sisesta oma tulemus");
+            char userResult = Convert.ToChar(Console.ReadLine().ToUpper());
+
+            switch (userResult)
+            {
+                case 'A':
+                    Console.WriteLine("Suurepärane");
+                    break;
+                case 'B':
+                    Console.WriteLine("Väga Hea");
+                    break;
+                case 'C':
+                    Console.WriteLine("Hea");
+                    break;
+                case 'D':
+                    Console.WriteLine("Rahuldav");
+                    break;
+                case 'E':
+                    Console.WriteLine("Kasin");
+                    break;
+                case 'F':
+                    Console.WriteLine("Puudulik");
+                    break;
+                default:
+                    Console.WriteLine($"Vale väärtus");
+                    break;
+
+            }
+
+            Console.WriteLine("Head päeva!");
+
+        }
+    }
+}
